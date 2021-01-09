27949e8287bb07dc9d14c0b98d78861faa53ba5e
laugil627 <77131507+laugil627@users.noreply.github.com>
2021-01-09 11:32:42 -0500

    Create ExtractTweets.py

---
+++ ExtractTweets.py
@@ -0,0 +1,39 @@
+import tweepy
+from credentials import *
+
+# authorization tokens
+# Keys to be entered
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+
+def twitter_setup():
+    # Twitter credentials authorization
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    return api
+
+
+def tweet_extract(api, username):
+    # initialize list of tweet
+    five_recent_tweets = []
+
+    tweets = api.user_timeline(screen_name=username, count=10, include_rts=False, tweet_mode='extended')
+
+    for tweet in tweets[:5]:
+        five_recent_tweets.append(tweet)
+
+    return five_recent_tweets
+
+
+def main():
+    userID = raw_input("Enter a Twitter handle: ")
+    api_to_use = twitter_setup()
+    tweet_extract(api_to_use, userID)
+
+if __name__ == "__main__":
+    main()
